6ab45d9d727534e085cb2479ecfbef333bba4947
Azsydykov <57948994+Azsydykov@users.noreply.github.com>
2022-09-02 18:13:55 +0600

    del setters

--- src/enums/Elements.java
+++ src/enums/Elements.java
@@ -19,26 +19,11 @@ public enum Elements {
     public String getSymbol() {
         return symbol;
     }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAtomNumber() {
         return atomNumber;
-    }
-
-    public void setAtomNumber(int atomNumber) {
-        this.atomNumber = atomNumber;
     }
 
     Elements(String name,String symbol, int atomNumber) {
